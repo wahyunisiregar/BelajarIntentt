80d28832fbe5ce62a4f2489b31ed513b108412df
wahyunisiregar <siregarwahyuni2@gmail.com>
2023-04-01 01:23:07 +0700

    Belajar Intent pemograman Mobile

---
+++ app/src/main/java/com/yuni/Cars.kt
@@ -0,0 +1,15 @@
+package com.yuni
+
+class Cars {
+
+    val plat: Any
+    val tahun: Any
+    val merk: Any
+}
+
+class cars : Parcelable {
+
+    val plat: Any
+    val tahun: Any
+    val merk: Any
+}
